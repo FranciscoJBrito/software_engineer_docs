c6c3f7ef028275248a5e9c0e3b39cfe94bc59929
Francisco Brito <franciscojavierbrito16@gmail.com>
2024-03-07 11:34:07 -0300

    adding sidebar ui to the hom page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,12 @@
 'use client'
 import FileTree from "./components/FileTree/FileTree";
 import styles from "./page.module.css";
+import Sidebar from "./ui/Sidebar/Sidebar";
 
 export default function Home() {
   return (
     <main className={styles.main}>
-      <FileTree />
+      <Sidebar />
     </main>
   );
 }
--- src/app/ui/Sidebar/Sidebar.tsx
+++ src/app/ui/Sidebar/Sidebar.tsx
@@ -1,9 +1,10 @@
 import styles from './Sidebar.module.css'
+import FileTree from '@/app/components/FileTree/FileTree'
 
 const Sidebar = () => {
   return (
     <div className={styles.main_container}>
-
+      <FileTree />
     </div>
   )
 }
